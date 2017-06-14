82cdf348c8c07f5ab5cc5d10e06495342c807f93
omkard3sai <omkar.desai@utdallas.edu>
2017-06-13 20:01:24 -0500

    Added sample

---
+++ Sample.py
@@ -0,0 +1,9 @@
+from DoublyLinkedList import DoublyLinkedList
+
+listObject = DoublyLinkedList()
+listObject.insertend('End')
+listObject.insertstart('Start')
+listObject.insertend('Modified End')
+listObject.displayforward()
+listObject.displaybackward()
+
